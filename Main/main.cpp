4cd52a07f27710e6e25682353e7d3a79242ee7ec
Andry Rafam Andrianjafy <41973268+AndryRafam@users.noreply.github.com>
2020-07-08 17:34:35 +0300

    Update main.cpp

--- Main/main.cpp
+++ Main/main.cpp
@@ -4,42 +4,22 @@
  
  
 #include <iostream>
-#include <sstream>
 #include "../Tools/header.h"
 
 int main(){
 	
 	DES des;
+	system("clear");
 	
-	std::string plaintext, bloc;
+	std::cout << "    ========================================    " << std::endl << std::endl;
+	std::cout << "         Andry RAFAM ANDRIANJAFY - TDES         " << std::endl << std::endl;
+	std::cout << "    ========================================    " << std::endl;
+					   
+	std::string plaintext;
 	std::cout << std::endl << "[ Clear Text ] -> ";
 	std::getline(std::cin,plaintext); 
 	
-	std::string hexa = "";
-	for(auto i(0); i < plaintext.size(); i++){
-		std::stringstream my_ps;
-		my_ps << std::uppercase << std::hex << int(plaintext[i]);
-		hexa+=my_ps.str();
-	}
-	if(hexa.size() < 16){ // If size of the hexa text is less than 16 (64 bits); pad with random hexadecimal character
-		std::cout << std::endl << "[ Encrypted Text ] -> ";
-		hexa = des.Padding(hexa);
-		std::cout << des.Triple_DES_Encrypt(hexa);
-		std::cout << std::endl << std::endl;
-	}
-	else if(hexa.size() > 16){ // ECB mode (Crack the message in 64 bits each - 16 hexadecimal numbers)
-		std::cout << std::endl << "[ Encrypted Text ] -> ";
-		for(auto i(0); i < hexa.size(); i+=16){
-			bloc = hexa.substr(i,16);
-			if(bloc.size() < 16){ // If size of the final bloc is less than 16 (64 bits); pad the bloc with random hexadecimal character
-				bloc = des.Padding(bloc);
-			}
-			std::cout << des.Triple_DES_Encrypt(bloc) << " ";
-		}
-		std::cout << std::endl << std::endl;
-	}
-	else{ // If hexa size == 16 (64 bits)
-		std::cout << std::endl << "[ Encrypted Text ] -> " << des.Triple_DES_Encrypt(hexa) << "  " << std::endl << std::endl;
-    }
-    return 0;
+	des.Func(plaintext);
+	
+	return 0;
 }
