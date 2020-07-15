2127c893c9bd3fed9c10a075deb65d111d60b73b
Andry Rafam Andrianjafy <41973268+AndryRafam@users.noreply.github.com>
2020-07-15 22:06:11 +0300

    Update main.cpp

--- Main/main.cpp
+++ Main/main.cpp
@@ -1,9 +1,13 @@
 /* Andry RAFAM ANDRIANJAFY - TDES (Triple Data Encryption Standard) program.
  * 
+ * OS: Linux Xubuntu 20.04 LTS (focal fossa)
+ * 
  * Compiler version: gcc/g++ 9.3.0 */
  
  
 #include <iostream>
+#include <algorithm>
+#include <ctime>
 #include "../Tools/header.h"
 
 int main(){
@@ -18,6 +22,14 @@ int main(){
 	std::string plaintext;
 	std::cout << std::endl << "[ Clear Text ] -> ";
 	std::getline(std::cin,plaintext); 
+
+	srand(time(0));
+	
+	// Fisher-Yates shuffle algorithm
+	for(auto i = plaintext.length()-1; i > 0; i--){
+		int j = rand()%(i+1);
+		std::swap(plaintext[i],plaintext[j]);
+	}
 	
 	des.Func(plaintext);
 	
